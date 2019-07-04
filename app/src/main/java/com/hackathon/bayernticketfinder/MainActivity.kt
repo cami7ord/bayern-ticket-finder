35135243221635f4f449ba8d2365af3aa3d99c01
Pawel Rak <pawel@scalable.capital>
2019-07-04 17:18:21 +0200

    Adds basic activity with fragment and vm

--- app/src/main/java/com/hackathon/bayernticketfinder/MainActivity.kt
+++ app/src/main/java/com/hackathon/bayernticketfinder/MainActivity.kt
@@ -2,11 +2,18 @@ package com.hackathon.bayernticketfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackathon.bayernticketfinder.ui.feed.FeedFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, FeedFragment.newInstance())
+                .commitNow()
+        }
     }
+
 }
---
+++ app/src/main/java/com/hackathon/bayernticketfinder/ui/feed/FeedViewModel.kt
@@ -0,0 +1,7 @@
+package com.hackathon.bayernticketfinder.ui.feed
+
+import androidx.lifecycle.ViewModel
+
+class FeedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
