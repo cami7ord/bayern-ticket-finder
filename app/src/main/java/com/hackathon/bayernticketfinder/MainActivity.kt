be5298bab1c1d5234777126d69b631aa7365b695
Camilo Baquero <camilo.jimenez@scalable.capital>
2019-07-05 12:08:21 +0200

    Basic Auth navigation

--- app/src/main/java/com/hackathon/bayernticketfinder/MainActivity.kt
+++ app/src/main/java/com/hackathon/bayernticketfinder/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.hackathon.bayernticketfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.hackathon.bayernticketfinder.ui.auth.AuthActivity
 import com.hackathon.bayernticketfinder.ui.feed.FeedFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, FeedFragment.newInstance())
-                .commitNow()
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, FeedFragment.newInstance())
+                    .commitNow()
+            }
+        } else {
+            val intent = Intent(this@MainActivity, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+
     }
 
 }
--- app/src/main/java/com/hackathon/bayernticketfinder/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/hackathon/bayernticketfinder/ui/auth/AuthActivity.kt
@@ -1,10 +1,12 @@
 package com.hackathon.bayernticketfinder.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import com.hackathon.bayernticketfinder.MainActivity
 import com.hackathon.bayernticketfinder.R
 import com.hackathon.bayernticketfinder.data.Result
 import kotlinx.android.synthetic.main.activity_auth.*
@@ -19,8 +21,6 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        //username.setText(viewModel.sayHello())
-
         login_button.setOnClickListener {
             loading.visibility = View.VISIBLE
             viewModel.login(
@@ -34,12 +34,13 @@ class AuthActivity : AppCompatActivity() {
 
             loading.visibility = View.GONE
             if (loginResult is Result.Error) {
-                //showLoginFailed(loginResult.error)
                 Toast.makeText(this, "Login Error", Toast.LENGTH_LONG).show()
             }
             if (loginResult is Result.Success) {
-                //updateUiWithUser(loginResult.success)
                 Toast.makeText(this, "Login Success", Toast.LENGTH_LONG).show()
+                val intent = Intent(this@AuthActivity, MainActivity::class.java)
+                startActivity(intent)
+                finishAfterTransition()
             }
 
         })
