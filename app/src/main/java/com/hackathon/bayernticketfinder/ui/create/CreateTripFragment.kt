502347dc50ea5c6f5e2c5cb1dcc9e54526e1fa60
Pawel Rak <pawel@scalable.capital>
2019-07-05 13:14:39 +0200

    CreateTripFragment

---
+++ app/src/main/java/com/hackathon/bayernticketfinder/ui/create/CreateTripFragment.kt
@@ -0,0 +1,68 @@
+package com.hackathon.bayernticketfinder.ui.create
+
+import android.app.DatePickerDialog
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hackathon.bayernticketfinder.R
+
+import kotlinx.android.synthetic.main.create_trip_fragment.*
+
+import java.util.*
+
+
+class CreateTripFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CreateTripFragment()
+    }
+
+    private lateinit var viewModel: CreateTripViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+        return inflater.inflate(R.layout.create_trip_fragment, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupLayout()
+    }
+
+    fun setupLayout() {
+
+        departure_time_edit.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            DatePickerDialog(activity, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                departure_time_edit.setText("$dayOfMonth/$monthOfYear/$year")
+            }, year, month, day).show()
+
+        }
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CreateTripViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
