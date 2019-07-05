520117546386531b5a6480dfbaa8528b385d60ac
Camilo Baquero <camilo.jimenez@scalable.capital>
2019-07-05 13:05:09 +0200

    Navigation

--- app/src/main/java/com/hackathon/bayernticketfinder/MainActivity.kt
+++ app/src/main/java/com/hackathon/bayernticketfinder/MainActivity.kt
@@ -1,30 +1,71 @@
 package com.hackathon.bayernticketfinder
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.hackathon.bayernticketfinder.extensions.setupWithNavController
 import com.hackathon.bayernticketfinder.ui.auth.AuthActivity
-import com.hackathon.bayernticketfinder.ui.feed.FeedFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
 
         if (FirebaseAuth.getInstance().currentUser != null) {
             if (savedInstanceState == null) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, FeedFragment.newInstance())
-                    .commitNow()
-            }
+                setupBottomNavigationBar()
+            }// Else, need to wait for onRestoreInstanceState
         } else {
             val intent = Intent(this@MainActivity, AuthActivity::class.java)
             startActivity(intent)
             finish()
         }
 
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navGraphIds = listOf(R.navigation.feed, R.navigation.mytrips, R.navigation.profile)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 
 }
